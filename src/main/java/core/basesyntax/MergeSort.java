f4fc4a7722668cb996845a1130240b222963c9d1
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-20 17:23:08 +0300

    test to method mergeSort()

---
+++ src/main/java/core/basesyntax/MergeSort.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class MergeSort {
+    /**
+     * Реализуйте метод int[] mergeSort(int[] array), который вернёт отсортированный
+     * массив целых чисел с помощью сортировки слиянием.
+     */
+    public int[] mergeSort(int[] array) {
+        return null;
+    }
+}
---
+++ src/test/java/core/basesyntax/MergeSortTest.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class MergeSortTest {
+    private static final int[][] array = {
+            {1, 3, 5, 7, 9, 3, 6, 8, 0, 2},
+            {-5, -3, -2, -6, 4, -14, 32, 11, -16, 6},
+            {9, 6, 4, 3, 2, 6, 1, 12, 3, 2},
+            {-10, 10, -9, 9, -8, 8, -4, 4, -1, 1},
+            {4, 2, -9, 6, -3, 0, 12, 34, 25, 11},
+    };
+
+    @Test
+    public void mergeSort() {
+        MergeSort sort = new MergeSort();
+        for (int i = 0; i < 5; i++) {
+            int[] actualResult = sort.mergeSort(array[i]);
+            int[] expectedResult = Arrays.stream(array[i]).sorted().toArray();
+            Assert.assertArrayEquals(
+                    "Test failed with array " + Arrays.toString(array[i]),
+                    expectedResult,
+                    actualResult);
+        }
+    }
+}
