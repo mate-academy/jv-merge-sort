31be7c9df108d14a7867244e62a4fdf1213f906b
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-21 15:12:05 +0300

    changed the name of test data

--- src/test/java/core/basesyntax/MergeSortTest.java
+++ src/test/java/core/basesyntax/MergeSortTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class MergeSortTest {
-    private static final int[][] array = {
+    private static final int[][] testArrays = {
             {1, 3, 5, 7, 9, 3, 6, 8, 0, 2},
             {-5, -3, -2, -6, 4, -14, 32, 11, -16, 6},
             {9, 6, 4, 3, 2, 6, 1, 12, 3, 2},
@@ -18,10 +18,10 @@ public class MergeSortTest {
     public void mergeSort() {
         MergeSort sort = new MergeSort();
         for (int i = 0; i < 5; i++) {
-            int[] actualResult = sort.mergeSort(array[i]);
-            int[] expectedResult = Arrays.stream(array[i]).sorted().toArray();
+            int[] actualResult = sort.mergeSort(testArrays[i]);
+            int[] expectedResult = Arrays.stream(testArrays[i]).sorted().toArray();
             Assert.assertArrayEquals(
-                    "Test failed with array " + Arrays.toString(array[i]),
+                    "Test failed with array " + Arrays.toString(testArrays[i]),
                     expectedResult,
                     actualResult);
         }
