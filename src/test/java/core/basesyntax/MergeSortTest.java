a613bab39ae3b8194cc099ae7728b08b234a9097
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-25 12:43:32 +0300

    created new helper method and got rid of duplicated arrays

--- src/test/java/core/basesyntax/MergeSortTest.java
+++ src/test/java/core/basesyntax/MergeSortTest.java
@@ -8,14 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MergeSortTest {
-    private static final int[][] defaultArrays = {
-            {1, 3, 5, 7, 9, 3, 6, 8, 0, 2},
-            {-5, -3, -2, -6, 4, -14, 32, 11, -16, 6},
-            {9, 6, 4, 3, 2, 6, 1, 12, 3, 2},
-            {-10, 10, -9, 9, -8, 8, -4, 4, -1, 1},
-            {4, 2, -9, 6, -3, 0, 12, 34, 25, 11},
-    };
-
     private static final int[][] testArrays = {
             {1, 3, 5, 7, 9, 3, 6, 8, 0, 2},
             {-5, -3, -2, -6, 4, -14, 32, 11, -16, 6},
@@ -24,28 +16,16 @@ public class MergeSortTest {
             {4, 2, -9, 6, -3, 0, 12, 34, 25, 11},
     };
 
+    private static final int[][] defaultArrays = testArrays.clone();
+
     @Test
     public void mergeSort() {
         MergeSort sort = new MergeSort();
         for (int i = 0; i < 5; i++) {
             sort.mergeSort(testArrays[i]);
             for (int j = 0; j < testArrays[i].length; j++) {
-                Map<Integer, Integer> expectedMap = new HashMap();
-                for (Integer number : defaultArrays[i]) {
-                    if (expectedMap.keySet().contains(number)) {
-                        expectedMap.put(number, expectedMap.get(number) + 1);
-                    } else {
-                        expectedMap.put(number, 1);
-                    }
-                }
-                Map<Integer, Integer> actualMap = new HashMap();
-                for (Integer number : testArrays[i]) {
-                    if (actualMap.keySet().contains(number)) {
-                        actualMap.put(number, actualMap.get(number) + 1);
-                    } else {
-                        actualMap.put(number, 1);
-                    }
-                }
+                Map<Integer, Integer> expectedMap = transformToHashMap(i);
+                Map<Integer, Integer> actualMap = transformToHashMap(i);
                 for (int l = 0; l < testArrays[i].length - 1; l++) {
                     Assert.assertTrue(
                             "Test failed. Array isn't sorted: " + Arrays.toString(testArrays[i]),
@@ -58,5 +38,17 @@ public class MergeSortTest {
                         actualMap);
             }
         }
+    }
+
+    private Map<Integer, Integer> transformToHashMap(int index) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer number : defaultArrays[index]) {
+            if (map.keySet().contains(number)) {
+                map.put(number, map.get(number) + 1);
+            } else {
+                map.put(number, 1);
+            }
+        }
+        return map;
     }
 }
